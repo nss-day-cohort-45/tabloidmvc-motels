039de740da1908bd8dff3ae8c008c9f3db5aa2b9
skgilmore <skg.gilmore@gmail.com>
2021-04-01 11:08:51 -0500

    Category option in nav bar and viewable

--- TabloidMVC/Repositories/CategoryRepository.cs
+++ TabloidMVC/Repositories/CategoryRepository.cs
@@ -14,7 +14,7 @@ namespace TabloidMVC.Repositories
                 conn.Open();
                 using (var cmd = conn.CreateCommand())
                 {
-                    cmd.CommandText = "SELECT id, name FROM Category";
+                    cmd.CommandText = "SELECT id, name FROM Category order by name ";
                     var reader = cmd.ExecuteReader();
 
                     var categories = new List<Category>();
