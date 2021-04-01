e30c64e768433c78d8cebab6b73b83ab73ba927f
Meghan Semrad <mlsemrad753@gmail.com>
2021-04-01 13:12:27 -0500

    saving to do PR review

---
+++ TabloidMVC/Models/Comment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TabloidMVC.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public int PostId { get; set; }
+        public int UserProfileId { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+        public struct DateTime : IComparable, IFormattable,
+    IConvertible, ISerializable, IComparable<DateTime>, IEquatable<DateTime>
+    }
+}
